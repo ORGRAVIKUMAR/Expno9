93b8ad7b91009cfac41841dc64893bee3e779d81
ORGRAVIKUMAR <111498241+ORGRAVIKUMAR@users.noreply.github.com>
2022-10-25 18:20:25 +0530

    Add files via upload

---
+++ Exno9/filecheck.java
@@ -0,0 +1,47 @@
+package demo;
+import java.io.*;
+import java.util.*;
+public class filecheck
+{
+    public static void main(String args[])throws Exception
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter your file name");
+        String name=s.nextLine();
+        File fin=new File(name);
+        if(fin.exists())
+        {
+            System.out.println("Your file is exists");
+        }
+        else
+        {
+            System.out.println("Your file is not exists please enter correct filename");
+        }
+        if(fin.canRead())
+        {
+            System.out.println("Your file is only readable file");
+        }
+        else if(fin.canWrite())
+        {
+            System.out.println("Your file is only writable file");
+        }
+        else
+        {
+            System.out.println("Your file can do both weite and read");
+        }
+        System.out.println(fin.length());
+        String name1=name;
+        char c2[]=name1.toCharArray();
+        for(int i=0;i<c2.length;i++)
+        {
+            if(c2[i]=='.')
+            {
+                for(int j=i+1;j<c2.length;j++)
+                {
+                    System.out.print(c2[j]);
+                }
+            }
+        }
+        
+    }
+}
